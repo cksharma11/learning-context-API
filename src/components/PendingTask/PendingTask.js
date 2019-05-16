0c55750eeb3eca98f52a042ac6030539080bd6fb
Chandan Kumar <22887527+cksharma11@users.noreply.github.com>
2019-05-16 09:35:51 +0530

    Refactor pending and completed task component

--- src/components/PendingTask/PendingTask.js
+++ src/components/PendingTask/PendingTask.js
@@ -1,21 +1,18 @@
 import React from "react";
 import { TaskContext } from "../../App";
 import Header from "../Header/Header";
+import TaskList from "../TaskList/TaskList";
 
 class PendingTask extends React.Component {
   render() {
     return (
       <TaskContext.Consumer>
         {context => {
-          const pendingTask = context.tasks.filter(task => !task.status);
+          const pendingTasks = context.tasks.filter(task => !task.status);
           return (
             <div>
               <Header />
-              {pendingTask.map(task => (
-                <div key={task.id} className="pending">
-                  {task.description}
-                </div>
-              ))}
+              <TaskList tasks={pendingTasks} />
             </div>
           );
         }}
